ca63aca3406c6f5d3a9e0ce162d7460c4f7977e6
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-09-08 12:18:51 -0500

    disable hud bar and replace heart textures events now only run for the main player

--- src/main/java/moriyashiine/strawberrylib/impl/mixin/event/client/disablehudbar/InGameHudMixin.java
+++ src/main/java/moriyashiine/strawberrylib/impl/mixin/event/client/disablehudbar/InGameHudMixin.java
@@ -5,30 +5,26 @@ package moriyashiine.strawberrylib.impl.mixin.event.client.disablehudbar;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import moriyashiine.strawberrylib.api.event.client.DisableHudBarEvent;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.entity.player.PlayerEntity;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
-public abstract class InGameHudMixin {
+public class InGameHudMixin {
 	@Unique
 	private boolean shouldDisableHudBar = false;
 
-	@Shadow
-	@Nullable
-	protected abstract PlayerEntity getCameraPlayer();
-
 	@Inject(method = "renderMainHud", at = @At("HEAD"))
 	private void slib$disableHudBar(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
-		shouldDisableHudBar = DisableHudBarEvent.EVENT.invoker().shouldDisableHudBar(getCameraPlayer()).get();
+		PlayerEntity player = MinecraftClient.getInstance().player;
+		shouldDisableHudBar = player != null && DisableHudBarEvent.EVENT.invoker().shouldDisableHudBar(player).get();
 	}
 
 	@ModifyExpressionValue(method = "renderMainHud", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;getCurrentBarType()Lnet/minecraft/client/gui/hud/InGameHud$BarType;"))
