9679ae5d12266282a74d79b07e9e60a4b15f11c4
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-10-17 14:04:17 -0500

    fix after damage including death event not taking final modify damage taken event changes into account

--- src/main/java/moriyashiine/strawberrylib/impl/mixin/event/afterdamageincludingdeath/LivingEntityMixin.java
+++ src/main/java/moriyashiine/strawberrylib/impl/mixin/event/afterdamageincludingdeath/LivingEntityMixin.java
@@ -5,6 +5,7 @@ package moriyashiine.strawberrylib.impl.mixin.event.afterdamageincludingdeath;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import moriyashiine.strawberrylib.api.event.AfterDamageIncludingDeathEvent;
+import moriyashiine.strawberrylib.api.event.ModifyDamageTakenEvent;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.world.ServerWorld;
@@ -17,6 +18,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class LivingEntityMixin {
 	@Inject(method = "damage", at = @At("TAIL"))
 	private void slib$afterDamageIncludingDeath(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir, @Local(ordinal = 1) float dealt, @Local(ordinal = 0) boolean blocked) {
-		AfterDamageIncludingDeathEvent.EVENT.invoker().afterDamage((LivingEntity) (Object) this, source, dealt, amount, blocked);
+		LivingEntity living = (LivingEntity) (Object) this;
+		float modifiedDamage = ModifyDamageTakenEvent.getModifiedDamage(ModifyDamageTakenEvent.Phase.FINAL, amount, world, source, living);
+		AfterDamageIncludingDeathEvent.EVENT.invoker().afterDamage(living, source, dealt, modifiedDamage, blocked);
 	}
 }
