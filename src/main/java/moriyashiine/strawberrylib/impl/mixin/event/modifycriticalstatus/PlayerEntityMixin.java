b862426044e747e9917abaa31b6e9b0902eba2b1
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-12-12 12:37:10 -0600

    cleaner modify critical status event mixin

--- src/main/java/moriyashiine/strawberrylib/impl/mixin/event/modifycriticalstatus/PlayerEntityMixin.java
+++ src/main/java/moriyashiine/strawberrylib/impl/mixin/event/modifycriticalstatus/PlayerEntityMixin.java
@@ -3,6 +3,7 @@
  */
 package moriyashiine.strawberrylib.impl.mixin.event.modifycriticalstatus;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import moriyashiine.strawberrylib.api.event.ModifyCriticalStatusEvent;
 import moriyashiine.strawberrylib.impl.common.StrawberryLib;
 import net.fabricmc.fabric.api.util.TriState;
@@ -10,16 +11,15 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
-	@ModifyVariable(method = "attack", at = @At("STORE"), ordinal = 2)
-	private boolean slib$modifyCriticalStatus(boolean value, Entity target) {
+	@ModifyReturnValue(method = "isCriticalHit", at = @At("RETURN"))
+	private boolean slib$modifyCriticalStatus(boolean original, Entity target) {
 		TriState state = ModifyCriticalStatusEvent.EVENT.invoker().isCritical((PlayerEntity) (Object) this, target, StrawberryLib.currentAttackCooldown);
 		if (state != TriState.DEFAULT) {
 			return state.get();
 		}
-		return value;
+		return original;
 	}
 }
