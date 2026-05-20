24e0b090bf8038e9bffe59bf75ea7879ecc4230d
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2026-05-20 16:47:40 -0500

    fix ModifyMovementEvents.MOVEMENT_DELTA sometimes causing moved wrongly errors

--- src/main/java/moriyashiine/strawberrylib/impl/mixin/event/modifymovement/EntityMixin.java
+++ src/main/java/moriyashiine/strawberrylib/impl/mixin/event/modifymovement/EntityMixin.java
@@ -7,6 +7,7 @@ package moriyashiine.strawberrylib.impl.mixin.event.modifymovement;
 import moriyashiine.strawberrylib.api.event.ModifyMovementEvents;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,9 +15,10 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(Entity.class)
 public class EntityMixin {
+	@SuppressWarnings("ConstantValue")
 	@ModifyVariable(method = "move", at = @At("HEAD"), argsOnly = true)
-	private Vec3 slib$modifyMovement(Vec3 delta) {
-		if ((Object) this instanceof LivingEntity living) {
+	private Vec3 slib$modifyMovement(Vec3 delta, MoverType moverType) {
+		if (moverType == MoverType.SELF && (Object) this instanceof LivingEntity living) {
 			return ModifyMovementEvents.MOVEMENT_DELTA.invoker().modify(delta, living);
 		}
 		return delta;
